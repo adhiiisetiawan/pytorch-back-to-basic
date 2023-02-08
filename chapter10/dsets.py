98b6c886f4c7d7dda653ebd5f370bc934e300754
adhiiisetiawan <adhisetiawan518@gmail.com>
2023-02-08 12:46:17 +0700

    preprocess candidates.csv

--- chapter10/dsets.py
+++ chapter10/dsets.py
@@ -25,3 +25,35 @@ def getCandidateInfoList(requireOnDisk_bool=True):
             diameter_dict.setdefault(series_uid, []).append(
                 (annotationCenter_xyz, annotationDiameter_mm)
             )
+
+    candidateInfo_list = []
+    with open('../dataset/candidates.csv', 'r') as f:
+        for row in list(csv.reader(f))[1:]:
+            series_uid = row[0]
+
+            if series_uid not in presentOnDisk_set and requireOnDisk_bool:
+                continue
+
+            isNodule_bool = bool(int(row[4]))
+            candidateCenter_xyz = tuple([float(x) for x in row[1:4]])
+
+            candidateDiameter_mm = 0.0
+            for annotation_tup in diameter_dict.get(series_uid, []):
+                annotationCenter_xyz, annotationDiameter_mm = annotation_tup
+                for i in range(3):
+                    delta_mm = abs(candidateCenter_xyz[i] - annotationDiameter_mm[i])
+                    if delta_mm > annotationDiameter_mm / 4:
+                        break
+                    else:
+                        candidateDiameter_mm = annotationDiameter_mm
+                        break
+
+                candidateInfo_list.append(CandidateInfoTuple(
+                    isNodule_bool,
+                    candidateDiameter_mm,
+                    series_uid,
+                    candidateCenter_xyz
+                ))
+
+            candidateInfo_list.sort(reverse=True)
+            return candidateInfo_list
