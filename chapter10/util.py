6577f9d13ba189f133d37387600f4ec4696b538f
adhiiisetiawan <adhisetiawan518@gmail.com>
2023-02-13 11:50:32 +0700

    create irc to xyz function

---
+++ chapter10/util.py
@@ -0,0 +1,13 @@
+import numpy as np
+from collections import namedtuple
+
+
+IrcTuple = namedtuple('IrcTuple', ['index', 'row', 'col'])
+XyzTuple = namedtuple('XyzTuple', ['x', 'y', 'z'])
+
+def irc2xyz(coord_irc, origin_xyz, vxSize_xyz, direction_a):
+    cri_a = np.array(coord_irc)[::-1] # swap the order when using numpy array
+    origin_a = np.array(origin_xyz)
+    vxSize_a = np.array(vxSize_xyz)
+    coords_xyz = (direction_a @ (cri_a * vxSize_a)) + origin_a
+    return XyzTuple(*coords_xyz)
