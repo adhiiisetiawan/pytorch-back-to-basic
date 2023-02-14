7219f9c2697afa48f9a519bb974391aab5e5dd42
adhiiisetiawan <adhisetiawan518@gmail.com>
2023-02-14 12:01:29 +0700

    add cache function

--- chapter10/disk.py
+++ chapter10/disk.py
@@ -21,3 +21,22 @@ class GzipDisk(Disk):
             value = str_io.getvalue()
 
         return super(GzipDisk, self).store(value, read)
+
+    def fetch(self, mode, filename, value, read):
+        value = super(GzipDisk, self).fetch(mode, filename, value, read)
+
+        if mode == MODE_BINARY:
+            str_io = BytesIO(value)
+            gz_file =  gzip.GzipFile(mode='rb', fileobj=str_io)
+            read_csio =BytesIO()
+
+            while True:
+                uncompressed_data = gz_file.read(2**30)
+                if uncompressed_data:
+                    read_csio.write(uncompressed_data)
+                else:
+                    break
+
+            value = read_csio.getvalue()
+        
+        return value
