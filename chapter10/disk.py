326138610c4f0f3b46a2b3baa41c5380f446cf60
adhiiisetiawan <adhisetiawan518@gmail.com>
2023-02-14 11:52:48 +0700

    create gzipdisk class and store function

--- chapter10/disk.py
+++ chapter10/disk.py
@@ -1,1 +1,23 @@
 import gzip
+
+from diskcache import FanoutCache, Disk
+from diskcache.core import BytesType, MODE_BINARY, BytesIO
+
+
+class GzipDisk(Disk):
+    def store(self, value, read, key=None):
+        if type(value) is BytesType:
+            if read:
+                value = value.read()
+                read = False
+            
+            str_io = BytesIO()
+            gz_file = gzip.GzipFile(mode='wb', compresslevel=1, fileobj=str_io)
+
+            for offset in range(0, len(value), 2**30):
+                gz_file.write(value[offset:offset+2**30])
+            gz_file.close()
+
+            value = str_io.getvalue()
+
+        return super(GzipDisk, self).store(value, read)
