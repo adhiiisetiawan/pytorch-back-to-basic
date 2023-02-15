93fe7cc1709b1cdfcb24ad9be980e3ed9ed92a7f
adhiiisetiawan <adhisetiawan518@gmail.com>
2023-02-15 16:24:11 +0700

    add arugment parser in init function

--- chapter11/training.py
+++ chapter11/training.py
@@ -1,8 +1,22 @@
+import argparse
+import datetime
+import sys
 
 
 class LunaTrainingApp:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, sys_argv=None):
+        if sys_argv is None:
+            sys_argv = sys.argv[1:]
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--num-workers',
+            help='Number of worker processes for background data loading',
+            default=8,
+            type=int
+        )
+        # ...........
+        self.cli_args = parser.parse_args(sys_argv)
+        self.time_str = datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
 
     def main(self):
         pass
